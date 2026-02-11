b857deb78260dc18460d599839cea92cebba9fe1
ljsoung <super15600@gmail.com>
2026-02-11 17:23:07 +0900

    init: fullstack project structure

---
+++ back-end/cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -0,0 +1,7 @@
+package com.packt.cardatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car,Long> { // 엔티티 클래스에 CRUD 기능 제공
+
+}
